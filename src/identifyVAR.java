61671705b57e9abe558c21290ea9be9fbd186a11
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-15 14:13:12 -0400

    first commit

---
+++ src/identifyVAR.java
@@ -0,0 +1,17 @@
+public class identifyVAR {
+    public static void main(String[] args) {
+        String Name;
+        String Place = "Nairobi";
+        int Num;
+        double D;
+        double Dollars = 103.84;
+        System.out.println("Hey Mom and Dad!\n" + " " + "Here are some interesting details about the first country I'm visiting\n");
+        System.out.println("Population Size: " + 49.7 + " " + "million\n");
+        System.out.println("Capital: " + "" + Place + "\n");
+        System.out.println("Currency: " + "" + "One U.S Dollar equals" + " $"+ "" + Dollars + " Kenyan Shillings!!\n");
+        System.out.println("I'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.");
+
+
+    }
+
+}
